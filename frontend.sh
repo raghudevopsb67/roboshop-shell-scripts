daa3430e027022de9e66ba0277acfefe29e6eeb0
Raghu K <raghuk.vit@gmail.com>
2022-09-15 06:45:28 +0530

    Variables in log file

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,10 @@
+LOG_FILE=/tmp/frontend
 echo Installing Nginx
-yum install nginx -y &>>/tmp/frontend
+yum install nginx -y &>>$LOG_FILE
 echo Status = $?
 
 echo Downloading Nginx Web Content
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 echo Status = $?
 
 cd /usr/share/nginx/html
@@ -13,16 +14,16 @@ rm -rf * &>>/tmp/frontend
 echo Status = $?
 
 echo Extracting Web Content
-unzip /tmp/frontend.zip &>>/tmp/frontend
+unzip /tmp/frontend.zip &>>$LOG_FILE
 echo Status = $?
 
 
-mv frontend-main/static/* . &>>/tmp/frontend
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
+mv frontend-main/static/* . &>>$LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 
 echo Starting Nginx Service
-systemctl enable nginx &>>/tmp/frontend
-systemctl restart nginx &>>/tmp/frontend
+systemctl enable nginx &>>$LOG_FILE
+systemctl restart nginx &>>$LOG_FILE
 echo Status = $?
 
 
