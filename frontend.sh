d6c5f32cb5d862736eab07f0adfd39e36512b86e
Raghu K <raghuk.vit@gmail.com>
2022-09-16 07:14:41 +0530

    Update roboshop-shell-scripts/

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,24 @@
 LOG_FILE=/tmp/frontend
+
+source common.sh
+
 echo Installing Nginx
 yum install nginx -y &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo Downloading Nginx Web Content
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 cd /usr/share/nginx/html
 
 echo Removing Old Web Content
 rm -rf * &>>/tmp/frontend
-echo Status = $?
+StatusCheck $?
 
 echo Extracting Web Content
 unzip /tmp/frontend.zip &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 
 mv frontend-main/static/* . &>>$LOG_FILE
@@ -24,6 +27,6 @@ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 echo Starting Nginx Service
 systemctl enable nginx &>>$LOG_FILE
 systemctl restart nginx &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 
--- mongodb.sh
+++ mongodb.sh
@@ -1,56 +1,42 @@
 LOG_FILE=/tmp/mongodb
 
-ID=$(id -u)
-if [ $ID -ne 0 ]; then
-  echo You should run this script as root user or with sudo privileges.
-  exit 1
-fi
-
-StatusCheck() {
-  if [ $1 -eq 0 ]; then
-    echo -e Status = "\e[32mSUCCESS\e[0m"
-  else
-    echo -e Status = "\e[31mFAILURE\e[0m"
-    exit 1
-  fi
-}
-
+source common.sh
 
 echo "Setting MongoDB Repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "Installing MongoDB Server"
 yum install -y mongodb-org &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "Update MongoDB Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo Status = $?
+StatusCheck $?
 
 echo "Starting MongoDB Service"
 systemctl enable mongod &>>$LOG_FILE
 systemctl restart mongod &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "Downloading MongoDb Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 cd /tmp
 echo "Extract Schema File"
 unzip mongodb.zip &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 cd mongodb-main
 
 echo "Load Catalogue Service Schema"
 mongo < catalogue.js &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "Load Users Service Schema"
 mongo < users.js &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 
 
