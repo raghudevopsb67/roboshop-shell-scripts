48f12e88fc9be4d99ee71d64162d5fb39c4d9b63
Raghu K <raghuk.vit@gmail.com>
2022-09-16 06:37:42 +0530

    Update roboshop-shell-scripts/

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,11 @@
 LOG_FILE=/tmp/catalogue
 
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo You should run this script as root user or with sudo privileges.
+  exit 1
+fi
+
 echo "Setup NodeJS Repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
