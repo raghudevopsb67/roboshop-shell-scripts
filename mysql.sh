732e0c3c18af8964dcc4a61a1839f6c934d33dfa
Raghu K <raghuk.vit@gmail.com>
2022-09-17 06:55:48 +0530

    Update roboshop-shell-scripts/

--- mysql.sh
+++ mysql.sh
@@ -26,7 +26,7 @@ echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}
 FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
 echo "Change the default root password"
-mysql -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
+mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
 StatusCheck $?
 
 
