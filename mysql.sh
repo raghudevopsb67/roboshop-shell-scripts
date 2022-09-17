b4cafa95b29ca1d666ed4f699cdaf182dd2f98c4
Raghu K <raghuk.vit@gmail.com>
2022-09-17 07:09:38 +0530

    Update roboshop-shell-scripts/

--- mysql.sh
+++ mysql.sh
@@ -32,12 +32,12 @@ if [ $? -ne 0 ]; then
   StatusCheck $?
 fi
 
-echo 'show plugins'| mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} | grep validate_password &>>$LOG_FILE
+echo 'show plugins'| mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} 2>/dev/null | grep validate_password &>>$LOG_FILE
 if [ $? -eq 0 ]; then
   echo "Uninstall Password Validation Plugin"
   echo "uninstall plugin validate_password;" | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
   StatusCheck $?
-fi 
+fi
 
 # > uninstall plugin validate_password;
 # curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
