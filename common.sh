31f45ec334ed23e6fa6b61a059b001c7d6d3ff52
Raghu K <raghuk.vit@gmail.com>
2022-09-19 06:30:55 +0530

    Update roboshop-shell-scripts/

--- common.sh
+++ common.sh
@@ -88,3 +88,20 @@ JAVA() {
 
   SYSTEMD_SETUP
 }
+
+PYTHON() {
+  echo "Install Python 3"
+  yum install python36 gcc python3-devel -y &>>${LOG_FILE}
+  StatusCheck $?
+
+  APP_PREREQ
+
+  cd /home/roboshop/${COMPONENT}
+
+  echo "Install Python Dependencies for APP``"
+  pip3 install -r requirements.txt &>>${LOG_FILE}
+  StatusCheck $?
+
+
+
+}
---
+++ payment.sh
@@ -0,0 +1,6 @@
+COMPONENT=payment
+LOG_FILE=/tmp/${COMPONENT}
+
+source common.sh
+PYTHON
+
