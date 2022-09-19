9ac0d5a0ca4fa944b6ceef9760c058852b935bec
Raghu K <raghuk.vit@gmail.com>
2022-09-19 06:45:33 +0530

    Update roboshop-shell-scripts/

--- frontend.sh
+++ frontend.sh
@@ -24,6 +24,10 @@ StatusCheck $?
 mv frontend-main/static/* . &>>$LOG_FILE
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 
+echo "Update RoboShop Config File"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+StatusCheck $?
+
 echo Starting Nginx Service
 systemctl enable nginx &>>$LOG_FILE
 systemctl restart nginx &>>$LOG_FILE
